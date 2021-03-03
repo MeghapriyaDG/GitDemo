719cdb81c101ee1fcbe92e54837213e4a90178cd
meghapriya <meghapriyadg@gmail.com>
2021-03-02 16:34:24 -0800

    First commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndriod()
+	{
+		System.out.println("NativeAndriod");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+}
